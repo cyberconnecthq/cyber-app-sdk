b25055df24d1cdef03c915ece84daeddebcc5484
Wei Su <me@weisu.io>
2024-03-19 10:27:50 +0800

    fix: update

--- src/CyberWalletConnector.ts
+++ src/CyberWalletConnector.ts
@@ -19,7 +19,12 @@ function cyberWalletConnector(parameters: CyberWalletConnectorParameters) {
   type Provider = CyberProvider | undefined;
   type Properties = {};
   type StorageItem = { "cyberWallet.disconnected": true };
-  let provider_: Provider | undefined;
+
+  const app = new CyberApp({
+    appId: appId || "",
+    name: name || "",
+    icon: icon || "",
+  });
 
   // const chainIds = chains?.map((chain) => chain.id);
   // const chains = Object.values(availableChains).filter(
@@ -53,99 +58,94 @@ function cyberWalletConnector(parameters: CyberWalletConnectorParameters) {
   //   return (error as ProviderRpcError).code === 4001;
   // };
 
-  return createConnector<Provider, Properties, StorageItem>((config) => ({
-    icon: "",
-    id: "cyberWallet",
-    name: "CyberWallet",
-    type: "cyberWallet",
-    async connect() {
-      const provider = await this.getProvider();
-      if (!provider) throw new ConnectorNotFoundError();
-      const accounts = await this.getAccounts();
-      const chainId = await this.getChainId();
-
-      provider.on("disconnect", this.onDisconnect.bind(this));
-
-      // Remove disconnected shim if it exists
-      if (shimDisconnect)
-        await config.storage?.removeItem("cyberWallet.disconnected");
-
-      return { accounts, chainId };
-    },
-    async disconnect() {
-      const provider = await this.getProvider();
-      if (!provider) throw new ProviderNotFoundError();
-
-      provider.removeListener("disconnect", this.onDisconnect.bind(this));
-
-      // Add shim signalling connector is disconnected
-      if (shimDisconnect)
-        await config.storage?.setItem("cyberWallet.disconnected", true);
-    },
-    async getAccounts() {
-      const provider = await this.getProvider();
-      if (!provider) throw new ProviderNotFoundError();
-      return (await provider.request({ method: "eth_accounts" })).map(
-        getAddress,
-      );
-    },
-    async getChainId() {
-      const provider = await this.getProvider();
-      if (!provider) throw new ProviderNotFoundError();
-      return normalizeChainId(provider.chainId);
-    },
-
-    getProvider: async () => {
-      const isInCyberWallet = isCyberWallet();
-
-      if (!isInCyberWallet) {
-        console.error("Could not load CyberWallet information.");
-      }
-
-      if (!appId) {
-        throw Error("CyberWalletConnector: appId is required");
-      }
-
-      const app = new CyberApp({
-        appId: appId,
-        name: name || "",
-        icon: icon || "",
-      });
-
-      // cyberwallet provider
-      const cyberProvider = new CyberProvider({
-        app: app,
-        chainId: config.chains[0].id, // default chain ID
-      });
-
-      provider_ = cyberProvider;
-      return provider_;
-    },
-    async isAuthorized() {
-      try {
-        const isDisconnected =
-          shimDisconnect &&
-          // If shim exists in storage, connector is disconnected
-          (await config.storage?.getItem("cyberWallet.disconnected"));
-        if (isDisconnected) return false;
-
+  return createConnector<Provider, Properties, StorageItem>((config) => {
+    return {
+      icon: "",
+      id: "cyberWallet",
+      name: "CyberWallet",
+      type: "cyberWallet",
+      async connect() {
+        const provider = await this.getProvider();
+        if (!provider) throw new ConnectorNotFoundError();
         const accounts = await this.getAccounts();
-        return !!accounts.length;
-      } catch {
-        return false;
-      }
-    },
-    onAccountsChanged() {
-      // Not relevant for Safe because changing account requires app reload.
-    },
-    onChainChanged(chain) {
-      const chainId = normalizeChainId(chain);
-      config.emitter.emit("change", { chainId });
-    },
-    onDisconnect() {
-      config.emitter.emit("disconnect");
-    },
-  }));
+        const chainId = await this.getChainId();
+
+        provider.on("disconnect", this.onDisconnect.bind(this));
+
+        // Remove disconnected shim if it exists
+        if (shimDisconnect)
+          await config.storage?.removeItem("cyberWallet.disconnected");
+
+        return { accounts, chainId };
+      },
+      async disconnect() {
+        const provider = await this.getProvider();
+        if (!provider) throw new ProviderNotFoundError();
+
+        provider.removeListener("disconnect", this.onDisconnect.bind(this));
+
+        // Add shim signalling connector is disconnected
+        if (shimDisconnect)
+          await config.storage?.setItem("cyberWallet.disconnected", true);
+      },
+      async getAccounts() {
+        const provider = await this.getProvider();
+        if (!provider) throw new ProviderNotFoundError();
+        return (await provider.request({ method: "eth_accounts" })).map(
+          getAddress,
+        );
+      },
+      async getChainId() {
+        const provider = await this.getProvider();
+        if (!provider) throw new ProviderNotFoundError();
+        return normalizeChainId(provider.chainId);
+      },
+
+      getProvider: async () => {
+        const isInCyberWallet = isCyberWallet();
+
+        if (!isInCyberWallet) {
+          console.error("Could not load CyberWallet information.");
+        }
+
+        if (!appId) {
+          throw Error("CyberWalletConnector: appId is required");
+        }
+
+        // cyberwallet provider
+        const cyberProvider = new CyberProvider({
+          app: app,
+          chainId: config.chains[0].id, // default chain ID
+        });
+
+        return cyberProvider;
+      },
+      async isAuthorized() {
+        try {
+          const isDisconnected =
+            shimDisconnect &&
+            // If shim exists in storage, connector is disconnected
+            (await config.storage?.getItem("cyberWallet.disconnected"));
+          if (isDisconnected) return false;
+
+          const accounts = await this.getAccounts();
+          return !!accounts.length;
+        } catch {
+          return false;
+        }
+      },
+      onAccountsChanged() {
+        // Not relevant for Safe because changing account requires app reload.
+      },
+      onChainChanged(chain) {
+        const chainId = normalizeChainId(chain);
+        config.emitter.emit("change", { chainId });
+      },
+      onDisconnect() {
+        config.emitter.emit("disconnect");
+      },
+    };
+  });
 }
 
 export default cyberWalletConnector;
