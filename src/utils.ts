56e4a453f78ffe29068af6037d7f766144dbb06e
HappySean <zhaoxiang2845@gmail.com>
2024-06-05 17:13:16 +0800

    fix: add more referrer

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,30 @@
-import { availableChains } from "./config/chains";
+import { availableChains } from './config/chains'
 
 export const isCyberWallet = () =>
-  typeof document !== "undefined" &&
+  typeof document !== 'undefined' &&
   [
-    "https://next.wallet.cyber.co/",
-    "https://wallet-sandbox.cyber.co/",
-    "https://wallet.cyber.co/",
-    "https://cyberwallet-sandbox-cyberconnect.vercel.app/",
-  ].includes(document.referrer);
+    'https://next.wallet.cyber.co/',
+    'https://wallet-sandbox.cyber.co/',
+    'https://wallet.cyber.co/',
+    'https://cyberwallet-sandbox-cyberconnect.vercel.app/',
+    'https://link3.to/',
+    'https://stg.link3.to/',
+    'https://next.link3.to/',
+  ].includes(document.referrer)
 
 export function isChainUnsupported(chainId: number) {
   return !Object.values(availableChains)
     .map((chain) => chain.id)
-    .includes(chainId);
+    .includes(chainId)
 }
 
 export function getSignerAddress() {
-  const storage = localStorage.getItem("cyber-app-sdk");
+  const storage = localStorage.getItem('cyber-app-sdk')
 
   if (!storage) {
-    return null;
+    return null
   }
 
-  const cyberAccountData = JSON.parse(storage);
-  return cyberAccountData.ownerAddress;
+  const cyberAccountData = JSON.parse(storage)
+  return cyberAccountData.ownerAddress
 }
